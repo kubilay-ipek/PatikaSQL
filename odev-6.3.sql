cbbb2f6f70dc81b2c2994dcc768de482f1123f31
Kubilay İpek <84242089+kubilay-ipek@users.noreply.github.com>
2022-12-09 22:11:07 +0300

    Add files via upload

---
+++ odev-6.3.sql
@@ -0,0 +1,5 @@
+-- film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+SELECT length FROM film
+WHERE rental_rate = 0.99
+ORDER BY length DESC
+LIMIT 1
